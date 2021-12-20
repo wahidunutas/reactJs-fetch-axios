795d64b185c042ba67d1b24132436c3ab07b5ed6
wahidunutas <wahidariesta@gmail.com>
2021-12-20 21:50:35 +0700

    react axios

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Home from "./Components/pages/Home";
+
+function App() {
+  return (
+    <>
+      <h1><center>Menampilkan Data Table</center></h1><hr></hr>
+      <Home />
+    </>
+  );
+}
+
+export default App;
